e5611fe1dd178bfcce0896209e65dddbe5f0bb6e
Sumedh <ssumedhiw@gmail.com>
2018-08-17 17:29:23 +0545

    added d3 package for its implementation in dashboard page

--- src/app/_helpers/jwt.interceptor.ts
+++ src/app/_helpers/jwt.interceptor.ts
@@ -8,7 +8,6 @@ export class JwtInterceptor implements HttpInterceptor {
     intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
         // add authorization header with jwt token if available
         const token = localStorage.getItem('token');
-       console.log(request.headers.get('map_url'));
        if (request.headers.get('map_url')){
         request = request.clone({
             url: environment.MAP_URL + request.url
--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { GoogleApiService } from '../_services/google-api.service';
+import * as d3 from 'd3';
 
 
 @Component({
@@ -9,19 +10,23 @@ import { GoogleApiService } from '../_services/google-api.service';
     styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
+    chart = {}
 
-
-    constructor(private googleapiService:GoogleApiService) {
+    constructor(
+        private googleapiService: GoogleApiService
+    ) {
     }
 
     ngOnInit() {
     }
+
     getlatlong(data: NgForm){
-        
-          
-        this.googleapiService.get_lat_long(data.value['address']).subscribe(response=>{
+        this.googleapiService.get_lat_long(data.value['address']).subscribe(response => {
             console.log(response);
         });
     }
-  
+
+    ngAfterContentInit() {
+      
+    }
 }
