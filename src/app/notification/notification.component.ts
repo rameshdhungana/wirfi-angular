2cf37e08e7686396cb3d3b4fddf623147f7e1646
Ramesh Dhungana <drameshiw@gmail.com>
2018-09-04 18:22:46 +0545

    [created] created notifications service and dynamic listing of types of notifications

--- src/app/notification/notification.component.ts
+++ src/app/notification/notification.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {NotificationService} from "../_services/notification.service";
 
 @Component({
     selector: 'app-notification',
@@ -9,11 +10,21 @@ export class NotificationComponent implements OnInit {
     public urgentCollapsed: boolean = false;
     public unreadCollapsed: boolean = false;
     public readCollapsed: boolean = false;
+    public urgentNotifications: Array<any> = [];
+    public unreadNotifications: Array<any> = [];
+    public readNotifications: Array<any> = [];
 
-    constructor() {
+    constructor(private notificationService: NotificationService) {
     }
 
     ngOnInit() {
+        this.notificationService.getAllNotification().subscribe(response => {
+            console.log(response);
+            this.urgentNotifications = response['data'];
+            this.unreadNotifications = response['data'];
+            this.readNotifications = response['data'];
+        })
+
     }
 
 }
