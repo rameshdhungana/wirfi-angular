ca6c427b17baab67f4769d744357aef751b757d4
Rohit Shrestha <srohitiw@gmail.com>
2018-09-07 10:23:14 +0545

    change in env.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,10 +1,6 @@
 export const environment = {
     production: true,
-<<<<<<< HEAD
     API_URL: 'http://wirfi.testiw.codesamaj.com:8001/',
     MAP_URL: 'https://maps.googleapis.com/maps/api/geocode/'
-=======
-    API_URL: 'http://api.wirfi.testiw.codesamaj.com'
->>>>>>> d7952f2f0d987948bcf1c5118a3205c3bebfb554
 
 };
