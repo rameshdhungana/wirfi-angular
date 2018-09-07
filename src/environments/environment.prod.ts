9306786b979e5f10acd31ead8ae1e9c70d5d1997
Rohit Shrestha <srohitiw@gmail.com>
2018-09-07 10:19:27 +0545

    added env varaible in prod env ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
     production: true,
-    API_URL: 'http://wirfi.testiw.codesamaj.com:8001/'
+    API_URL: 'http://wirfi.testiw.codesamaj.com:8001/',
+    MAP_URL: 'https://maps.googleapis.com/maps/api/geocode/'
 
 };
