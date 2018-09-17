3fffe9beabca57a37c03db00e80b642e522e9997
Sumedh <ssumedhiw@gmail.com>
2018-09-17 17:43:52 +0545

    fixed merge issue for line graph.\

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -329,9 +329,6 @@ data_new =  {
     this.noOfDevice(this.doughnut_filter_data);
   }
 
-
-
-
   createLineGraph(data) {
     d3.select('#line_chart svg').remove();
     const margin = { top: 20, right: 150, bottom: 30, left: 50 };
@@ -372,39 +369,39 @@ data_new =  {
       .append('g')
       .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
     svg.append('text')
-    .attr('x', (width+ 40))
+    .attr('x', (width + 40))
     .attr('y', (margin.top + 80 ))
     .style('fill', 'white')
     .style('font-size', '16px')
     .text('3 Online');
 
     svg.append('text')
-    .attr('x', (width+40))
-    .attr('y', (margin.top +100 ))
+    .attr('x', (width + 40))
+    .attr('y', (margin.top + 100 ))
     .style('fill', 'white')
     .style('font-size', '16px')
     .style('z-index', '1300')
     .text('3 Cell');
 
     svg.append('text')
-    .attr('x', (width+40))
-    .attr('y', (margin.top +120 ))
+    .attr('x', (width + 40))
+    .attr('y', (margin.top + 120 ))
     .style('fill', 'white')
     .style('font-size', '16px')
     .style('z-index', '1300')
     .text('3 Auto Recover');
 
     svg.append('text')
-    .attr('x', (width+40))
-    .attr('y', (margin.top +140 ))
+    .attr('x', (width + 40))
+    .attr('y', (margin.top + 140 ))
     .style('fill', 'white')
     .style('font-size', '16px')
     .style('z-index', '1300')
     .text('3 Weak Signal');
 
     svg.append('text')
-    .attr('x', (width+40))
-    .attr('y', (margin.top +160 ))
+    .attr('x', (width + 40))
+    .attr('y', (margin.top + 160 ))
     .style('fill', 'white')
     .style('font-size', '16px')
     .style('z-index', '1300')
@@ -492,16 +489,16 @@ data_new =  {
                       .style('left', (d3.event.pageX) + 'px')
                       .style('top', (d3.event.pageY - 28) + 'px');
                   })
-              .on('mouseout', function(d) {
+                .on('mouseout', function(d) {
                   div.transition()
                       .duration(500)
                       .style('opacity', 0);
-              })
+                })
                 .style('stroke', function () { // Add the colour
                   return daCurve['color'] = line_color;
                 })
                 .attr('d', d3.line<any>()
-                  .curve(stepRoundAfter)
+                  .curve(d3.curveStepAfter)
                   .x(function (d) {
                     // add circle in path
                     // svg.append('circle')
@@ -525,7 +522,7 @@ data_new =  {
                     console.log(x(d['date']));
                     return x((d['date'])); })
                   .y(function (d) { return y(d['status_1']); })
-                )
+                );
             //     .on('mouseover', function(d) {
             //       console.log(d['date']);
             //       div.transition()
@@ -590,7 +587,7 @@ data_new =  {
                 .select('line') // grab the tick line
                 .attr('class', 'quadrantBorder') // style with a custom class and CSS
                 .style('stroke-width', 0.5) // or style directly with attributes or inline styles
-                .style('stroke', '#232938' );
+                .style('stroke', '#232938');
             }
           }
 
@@ -698,88 +695,80 @@ data_new =  {
         .style('font-size', '18px');
   }
 
-  function Step(context, t) {
-    this._context = context;
-    this._t = t;
-  }
-
-  Step.prototype = {
-      areaStart: function() {
-          this._line = 0;
-      },
-      areaEnd: function() {
-          this._line = NaN;
-      },
-      lineStart: function() {
-          this._x = this._y = NaN;
-          this._point = 0;
-      },
-      lineEnd: function() {
-          if (0 < this._t && this._t < 1 && this._point === 2) {
-          this._context.lineTo(this._x, this._y);
-          }
-
-          if (this._line || (this._line !== 0 && this._point === 1)) {
-          this._context.closePath();
-          }
-          if (this._line >= 0) {
-          this._t = 1 - this._t, this._line = 1 - this._line;
-          }
-      },
-
-      point: function(x, y) {
-          x = +x, y = +y;
-          switch (this._point) {
-          case 0:
-              this._point = 1;
-              this._line ? this._context.lineTo(x, y) : this._context.moveTo(x, y);
-              break;
-          case 1:
-              this._point = 2; // proceed
-              break;
-          default:
-              {
-                  let xN, yN, mYb, mYa;
-                  if (this._t <= 0) {
-                      xN = Math.abs(x - this._x) * 0.25;
-                      yN = Math.abs(y - this._y) * 0.25;
-                      mYb = (this._y < y) ? this._y + yN : this._y - yN;
-                      mYa = (this._y > y) ? y + yN : y - yN;
-
-                      this._context.quadraticCurveTo(this._x, this._y, this._x, mYb);
-                      this._context.lineTo(this._x, mYa);
-                      this._context.quadraticCurveTo(this._x, y, this._x + xN, y);
-                      this._context.lineTo(x - xN, y);
-
-                  } else {
-                      const x1 = this._x * (1 - this._t) + x * this._t;
-
-                      xN = Math.abs(x - x1) * 0.25;
-                      yN = Math.abs(y - this._y) * 0.25;
-                      mYb = (this._y < y) ? this._y + yN : this._y - yN;
-                      mYa = (this._y > y) ? y + yN : y - yN;
-
-                      this._context.quadraticCurveTo(x1, this._y, x1, mYb);
-                      this._context.lineTo(x1, mYa);
-                      this._context.quadraticCurveTo(x1, y, x1 + xN, y);
-                      this._context.lineTo(x - xN, y);
-                  }
-                  break;
-              }
-          }
-          this._x = x, this._y = y;
-      }
-  };
-
-  let stepRound = function(context) {
-      return new Step(context, 0.5);
-  };
-
-  let stepRoundBefore = function(context) {
-      return new Step(context, 0);
-  };
-
-  let stepRoundAfter = function(context) {
-      return new Step(context, 1);
-  };
+  // function Step(context, t) {
+  //   this._context = context;
+  //   this._t = t;
+  // }
+  //
+  // Step.prototype = {
+  //     areaStart: function() {
+  //         this._line = 0;
+  //     },
+  //     areaEnd: function() {
+  //         this._line = NaN;
+  //     },
+  //     lineStart: function() {
+  //         this._x = this._y = NaN;
+  //         this._point = 0;
+  //     },
+  //     lineEnd: function() {
+  //         if (0 < this._t && this._t < 1 && this._point === 2) {
+  //         this._context.lineTo(this._x, this._y);
+  //         }
+  //
+  //         if (this._line || (this._line !== 0 && this._point === 1)) {
+  //         this._context.closePath();
+  //         }
+  //         if (this._line >= 0) {
+  //         this._t = 1 - this._t, this._line = 1 - this._line;
+  //         }
+  //     },
+  //
+  //     point: function(x, y) {
+  //         x = +x, y = +y;
+  //         switch (this._point) {
+  //         case 0:
+  //             this._point = 1;
+  //             this._line ? this._context.lineTo(x, y) : this._context.moveTo(x, y);
+  //             break;
+  //         case 1:
+  //             this._point = 2; // proceed
+  //             break;
+  //         default:
+  //             {
+  //                 let xN, yN, mYb, mYa;
+  //                 if (this._t <= 0) {
+  //                     xN = Math.abs(x - this._x) * 0.25;
+  //                     yN = Math.abs(y - this._y) * 0.25;
+  //                     mYb = (this._y < y) ? this._y + yN : this._y - yN;
+  //                     mYa = (this._y > y) ? y + yN : y - yN;
+  //
+  //                     this._context.quadraticCurveTo(this._x, this._y, this._x, mYb);
+  //                     this._context.lineTo(this._x, mYa);
+  //                     this._context.quadraticCurveTo(this._x, y, this._x + xN, y);
+  //                     this._context.lineTo(x - xN, y);
+  //
+  //                 } else {
+  //                     const x1 = this._x * (1 - this._t) + x * this._t;
+  //
+  //                     xN = Math.abs(x - x1) * 0.25;
+  //                     yN = Math.abs(y - this._y) * 0.25;
+  //                     mYb = (this._y < y) ? this._y + yN : this._y - yN;
+  //                     mYa = (this._y > y) ? y + yN : y - yN;
+  //
+  //                     this._context.quadraticCurveTo(x1, this._y, x1, mYb);
+  //                     this._context.lineTo(x1, mYa);
+  //                     this._context.quadraticCurveTo(x1, y, x1 + xN, y);
+  //                     this._context.lineTo(x - xN, y);
+  //                 }
+  //                 break;
+  //             }
+  //         }
+  //         this._x = x, this._y = y;
+  //     }
+  // };
+  //
+  // const stepRoundAfter = function(context) {
+  //     return new Step(context, 1);
+  // };
 }
