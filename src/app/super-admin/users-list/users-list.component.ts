7592521d2d9d85c98f4e3504a53f4fd348ef14f2
Sumedh <ssumedhiw@gmail.com>
2018-12-06 12:58:54 +0545

    Impersonate from users list page [Super admin]

--- src/app/super-admin/users-list/users-list.component.ts
+++ src/app/super-admin/users-list/users-list.component.ts
@@ -1,9 +1,11 @@
+import { Router } from '@angular/router';
 import {Component, OnInit} from '@angular/core';
 import {UserService} from '../../_services/user.service';
 import {MessageService} from '../../_services/message.service';
-import {DashboardService} from '../../_services/dashboard.service';
 import {MaterialDialogService} from '../../_services/material-dialog.service';
 import {DeleteUserComponent} from '../delete-user/delete-user.component';
+import { ImpersonateService } from '../../_services/impersonate.service';
+import { AuthenticationService } from '../../_services/authentication.service';
 
 @Component({
     selector: 'app-users-list',
@@ -15,7 +17,9 @@ export class UsersListComponent implements OnInit {
 
     constructor(private userService: UserService,
                 private messageService: MessageService,
-                private dashboardService: DashboardService,
+                private impersonateService: ImpersonateService,
+                private router: Router,
+                private authService: AuthenticationService,
                 private dialogService: MaterialDialogService) {
     }
 
@@ -34,6 +38,22 @@ export class UsersListComponent implements OnInit {
             response => {
                 this.messageService.add(response['message']);
                 this.getUser({});
+            }
+        );
+    }
+
+    impersonate(user_id) {
+        this.impersonateService.impersonateUser(user_id).subscribe(
+            response => {
+                localStorage.setItem('token', response['data']['user_token']);
+                localStorage.setItem('is_impersonating', 'true');
+                localStorage.setItem('personator', response['data']['impersonator']);
+                this.authService.ToggleImpersonate(true);
+                this.authService.me().subscribe(
+                    res => {
+                        this.router.navigateByUrl('dashboard');
+                    }
+                );
             }
         );
     }
