f94ad6242c84990b66bb0c045a32b09ece4909f5
Sumedh <ssumedhiw@gmail.com>
2018-12-06 13:00:16 +0545

    End impersonating. Added item to sidebar replacing logout if impersonating

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,6 +1,8 @@
 import {Component, OnInit, Input, Output, EventEmitter} from '@angular/core';
 import {AuthenticationService} from '../_services/authentication.service';
 import {environment} from '../../environments/environment';
+import { ImpersonateService } from '../_services/impersonate.service';
+import { Router } from '@angular/router';
 
 
 export class AppModule {
@@ -17,21 +19,48 @@ export class SidebarComponent implements OnInit {
     @Input() isCollapseContent;
     @Output() isCollapseContentChange = new EventEmitter();
 
-    public logged_in_user: any;
-    public URL = environment.API_URL;
+    logged_in_user: any;
+    is_impersonating;
+    URL = environment.API_URL;
 
-    constructor(private authService: AuthenticationService) {
-    }
+    constructor(
+        private authService: AuthenticationService,
+        private impersonateService: ImpersonateService,
+        private router: Router
+    ) {}
 
     ngOnInit() {
+        this.authService.ImpersonateObs().subscribe(
+            res => {
+                this.is_impersonating = res;
+            }
+        );
         this.authService.me().subscribe(response => {
             this.logged_in_user = response['data'];
         });
     }
+
     ShowText() {
         this.isVisible = true;
     }
+
     hideText() {
         this.isVisible = false;
+    }
+
+    endImpersonation() {
+        this.impersonateService.stopImpersonation().subscribe(
+            response => {
+                this.authService.ToggleImpersonate(false);
+                localStorage.setItem('token', response['data']['key']);
+                localStorage.removeItem('personator');
+                this.authService.me();
+                this.authService.ImpersonateObs().subscribe(
+                    res => {
+                        this.router.navigateByUrl('admin-dashboard');
+                    }
+                );
+            }
+        );
     }
 }
