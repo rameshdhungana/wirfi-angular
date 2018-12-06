e6e97ef4c6034e3ce6e8c27f17d53116e8e10bde
Sumedh <ssumedhiw@gmail.com>
2018-12-06 14:16:26 +0545

    change in user list page.

--- src/app/super-admin/admin-activity-log/admin-activity-log.component.ts
+++ src/app/super-admin/admin-activity-log/admin-activity-log.component.ts
@@ -1,6 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {UserService} from "../../_services/user.service";
-import {AdminActivityLogService} from "../../_services/admin-activity-log.service";
+import {AdminActivityLogService} from '../../_services/admin-activity-log.service';
 
 @Component({
     selector: 'app-admin-activity-log',
@@ -18,21 +17,15 @@ export class AdminActivityLogComponent implements OnInit {
 
     getLogList(queryparams) {
         this.adminLogService.getAdminActivityLogList(queryparams).subscribe(response => {
-            console.log(response);
-            // this.next = response['data']['next'];
-            // this.previous = response['data']['previous'];
             this.activityLogs = response['data'];
         });
-
     }
 
     ngOnInit() {
         this.getLogList({});
     }
 
-
     filterLogs(search_string) {
-        this.getLogList({search: search_string})
-
+        this.getLogList({search: search_string});
     }
 }
