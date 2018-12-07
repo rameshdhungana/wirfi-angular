592b5e5163130e1da8916b90f54cf37e1eb2308d
Sumedh <ssumedhiw@gmail.com>
2018-12-07 16:27:06 +0545

    fix in profile picture display

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,7 +1,8 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, Sanitizer} from '@angular/core';
 import {environment} from '../../environments/environment';
 import {AuthenticationService} from '../_services/authentication.service';
 import {BussinessService} from '../_services/bussiness.service';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
     selector: 'app-profile',
@@ -13,15 +14,19 @@ export class ProfileComponent implements OnInit {
     public URL = environment.API_URL;
     person: object;
     public businessInfo: any;
+    public image;
 
     constructor(private authService: AuthenticationService,
-                private  businessService: BussinessService) {
+                private  businessService: BussinessService,
+                private sanitization: DomSanitizer) {
     }
 
     ngOnInit() {
         this.authService.me().subscribe(response => {
             this.person = response['data'];
             this.loading = true;
+            this.image = this.sanitization.bypassSecurityTrustStyle(`url(${this.URL}${this.person['profile']['profile_picture']})`);
+            console.log(this.image);
         });
         this.businessService.getBusiness().subscribe(businessResp => {
             this.businessInfo = businessResp['data']['business_info'];
