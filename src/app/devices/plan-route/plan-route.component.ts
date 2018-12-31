f1c9e31b1dedc5e2687969c0490d29e208119e24
Ramesh Dhungana <drameshiw@gmail.com>
2018-12-31 13:14:58 +0545

    [update] plan route component modified to plot points being based on waypoints

--- src/app/devices/plan-route/plan-route.component.ts
+++ src/app/devices/plan-route/plan-route.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {DashboardService} from "../../_services/dashboard.service";
+import {BehaviorSubject, Subject} from "rxjs/Rx";
 
 @Component({
     selector: 'app-plan-route',
@@ -11,9 +12,13 @@ export class PlanRouteComponent implements OnInit {
     lng: number = -80.116577;
     planRouteLatLngList = [];
     routeCounter = [];
+    public deviceLocationList: any;
     public deviceLocations;
     public getDirectionButtonClicked = false;
-
+    public directionOptions: any;
+    public selectedDirectionList;
+    origin;
+    destination;
 
     constructor(private dashBoardSerVice: DashboardService) {
     }
@@ -21,50 +26,96 @@ export class PlanRouteComponent implements OnInit {
     ngOnInit() {
 
         this.dashBoardSerVice.getDeviceLocation().subscribe(locationResponse => {
+            //used for initializtion of device locations, and will not be altered
             this.deviceLocations = locationResponse;
-            console.log('this is the list of the devices loccation', this.deviceLocations);
+            //this will be altered , the property will be added and changed for choosing the image type(selected or non selected type)
+            this.deviceLocationList = locationResponse;
+            for (let device of this.deviceLocationList) {
+                device.is_selected = false;
+            }
+            //this is to disable the marker option produced due to agm-direction , because we use our custom marker through .png image
+            // included in assets/images
+            this.directionOptions = {
+                suppressMarkers: true,
+                preserveViewport: false
+            };
+
+            console.log('this is the list of the devices location', this.deviceLocationList);
 
 
         })
+
     }
 
-    addPointToPlanRoute(location, index) {
+    addPointToPlanRoute(location) {
+        this.getDirectionButtonClicked = false;
+        console.log(location);
+        const index = this.planRouteLatLngList.findIndex(x => x.lat == location.latitude && x.lng == location.longitude);
+        //we will use below deviceIndex to get devicelocation object and change selected and non selected icon
+        const deviceIndex = this.deviceLocationList.findIndex(x => x.latitude == location.latitude && x.longitude == location.longitude);
+        console.log(this.deviceLocations, 'gap ', this.deviceLocationList, deviceIndex, 'this is device index');
 
-        if (this.planRouteLatLngList.length < this.deviceLocations.length) {
-            console.log(location);
-            const index = this.planRouteLatLngList.findIndex(x => x.lat == location.latitude && x.lng == location.longitude);
-            console.log('this is index to check if the location already exits in array', index);
-            if (index === -1) {
+        console.log('this is index to check if the location already exits in array', index);
+        if (index === -1) {
 
-                this.planRouteLatLngList.push({lat: location.latitude, lng: location.longitude});
-                if (this.planRouteLatLngList.length > 1) {
-                    this.routeCounter.push(this.planRouteLatLngList.length - 2);
-                }
-
-            } else {
-
-                this.planRouteLatLngList.splice(index, 1);
-                this.routeCounter.pop();
-            }
-
-            console.log(this.routeCounter, 'this is couter coutn');
+            this.planRouteLatLngList.push({lat: location.latitude, lng: location.longitude});
+            //this makes this device object selected to change icon to selected one
+            console.log(this.deviceLocationList[deviceIndex], 'this is the device object in if');
+            this.deviceLocationList[deviceIndex].is_selected = true;
 
             if (this.planRouteLatLngList.length > 1) {
-                for (let index of this.routeCounter) {
-                    console.log('inside route loop origin', index, this.planRouteLatLngList[index]);
-                    console.log('inside route loop destination', index + 1, this.planRouteLatLngList[index + 1])
-                }
+                this.routeCounter.push(this.planRouteLatLngList.length - 2);
             }
 
+        } else {
 
-            console.log(this.planRouteLatLngList);
+            this.planRouteLatLngList.splice(index, 1);
+            this.routeCounter.pop();
+            //this make device object unselected to change icon to change icon to unselected one
+            console.log(this.deviceLocationList[deviceIndex], 'this is the device object in else ');
+
+            this.deviceLocationList[deviceIndex].is_selected = false;
 
 
         }
+
+        console.log(this.routeCounter, 'this is couter coutn');
+
+        if (this.planRouteLatLngList.length > 1) {
+            for (let index of this.routeCounter) {
+                console.log('inside route loop origin', index, this.planRouteLatLngList[index]);
+                console.log('inside route loop destination', index + 1, this.planRouteLatLngList[index + 1])
+            }
+        }
+
+
+        console.log(this.planRouteLatLngList, 'this is planned list');
+        console.log(this.selectedDirectionList, 'this is selected list')
+
+
     }
 
     getDirection() {
         this.getDirectionButtonClicked = true;
-    }
 
+        this.selectedDirectionList = [];
+        this.origin = this.planRouteLatLngList[0];
+        this.destination = this.planRouteLatLngList[this.planRouteLatLngList.length - 1];
+
+        //remove first and last (origin and destination and get waypoints in between for route)
+        const wayPoints = this.planRouteLatLngList.slice(1, this.planRouteLatLngList.length - 1);
+
+        for (let loc of wayPoints) {
+            this.selectedDirectionList.push({
+                location: {lat: loc.lat, lng: loc.lng},
+                stopover: true,
+            });
+
+            console.log(this.selectedDirectionList, 'this is selected list in get direction', 'refresh');
+
+            // this.deviceLocations = this.planRouteLatLngList;
+
+
+        }
+    }
 }
